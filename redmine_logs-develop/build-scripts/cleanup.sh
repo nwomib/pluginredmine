7395530ebe02fd0b1aa5596197ac31400e72f925
nwomib <nwomib@yandex.ru>
2022-10-12 00:27:36 +0600

    new_plugin

--- redmine_logs-develop/build-scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd `dirname $0`
-. env.sh
-cd ..
-
-rm -rf $TESTSPACE
--- redmine_logs-develop/build-scripts/env.sh
+++
@@ -1,5 +0,0 @@
-export TRAVISDIR=`pwd`
-export PATH_TO_PLUGIN=`dirname ${TRAVISDIR}`
-export TESTSPACE=$PATH_TO_PLUGIN/testspace
-export PATH_TO_REDMINE=$TESTSPACE/redmine
-export RAILS_ENV=test
